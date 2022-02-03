96de0968330e2de80242a275c191fda9a15ba2a0
Aikosh <yaza5ym71@gmail.com>
2022-02-03 16:30:57 +0900

    Improve some sentences

--- sketch_analogoutput.ino
+++ sketch_analogoutput.ino
@@ -1,11 +1,24 @@
+//For Aruino Mega2560
 
-const int outputAPin1  = 3;
-const int outputAPin2  = 5;
-double voltage1 = 1;
-double voltage2 = 2;
-double factor  =  51;
-double val1     = voltage1 * factor;
-double val2     = voltage2 * factor;
+//出力させるピン番号(PWM出力はディジタルピンのみ出力)
+//出力させたい電圧値
+//----##### CH1 ##### ------------------------------------
+const int outputPin1 = 3;
+double voltage1 = 1.6; //Dueで出力したい値（Megaのみで2.5V出力になる[127]）
+double Voffset1 = 1.563; // for correction between Mega and Due
+
+//----##### CH2 ##### ------------------------------------
+const int outputPin2 = 5;
+double voltage2 = 1.;
+double Voffset2 = 1.67; // for correction between Mega and Due
+
+
+double factor  =  51.; // this is 8 bit: 255(2^8-1) / 5V = 51
+
+double val1     = voltage1 * factor * Voffset1 ;
+double val2     = voltage2 * factor * Voffset2 ;
+
+double Vdue_th = 3.3;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,20 +29,27 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
- 
+  double Vin1 = voltage1 * Voffset1;
+  double Vin2 = voltage2 * Voffset2;
+  
+  if (Vin1<=Vdue_th) {
+    analogWrite(outputPin1, val1);
+  } else {analogWrite(outputPin1, 0);}
+  
+  if (Vin2<=Vdue_th) {
+    analogWrite(outputPin2, val2);
+  } else {analogWrite(outputPin2, 0);}
 
-  analogWrite(outputAPin1, 127);
-  analogWrite(outputAPin2, val2);
   
   Serial.print("pin1 = ");
-  Serial.print(outputAPin1);
+  Serial.print(outputPin1);
   Serial.print("\t output1 = ");
   Serial.print(voltage1);
   Serial.print("[V] value1 = ");
   Serial.println(val1);
 
   Serial.print("pin2 = ");
-  Serial.print(outputAPin2);
+  Serial.print(outputPin2);
   Serial.print("\t output2 = ");
   Serial.print(voltage2);
   Serial.print("[V] value2 = ");
