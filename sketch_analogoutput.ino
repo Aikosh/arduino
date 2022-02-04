6788814794d073d8752a3072a103709a9c7549ac
Aikosh <yaza5ym71@gmail.com>
2022-02-04 12:08:58 +0900

    Add the function of the switch button

--- sketch_analogoutput.ino
+++ sketch_analogoutput.ino
@@ -4,14 +4,21 @@
 //出力させたい電圧値
 //----##### CH1 ##### ------------------------------------
 const int outputPin1 = 3;
-double voltage1 = 1.6; //Dueで出力したい値（Megaのみで2.5V出力になる[127]）
+double voltage1 = 1.; //
+//double voltage1 = 1.6; //Dueで出力したい値（Megaのみで2.5V出力になる[127]）
 double Voffset1 = 1.563; // for correction between Mega and Due
 
 //----##### CH2 ##### ------------------------------------
 const int outputPin2 = 5;
-double voltage2 = 1.;
-double Voffset2 = 1.67; // for correction between Mega and Due
+double voltage2 = 2.;
+double Voffset2 = 1.563; // for correction between Mega and Due
 
+//----##### CH3 ##### ------------------------------------
+//const int outputPin3 = 7;
+//double voltage3 = 2.4;
+//double Voffset3 = 1.67; // for correction between Mega and Due
+
+//----------------------------------------------------------
 
 double factor  =  51.; // this is 8 bit: 255(2^8-1) / 5V = 51
 
@@ -20,26 +27,47 @@ double val2     = voltage2 * factor * Voffset2 ;
 
 double Vdue_th = 3.3;
 
+
+//---#####Switch#####---------------------------------------
+const int switchPin = 13;
+int val = 0;//ボタンの初期値はオフ
+int old_val = 0;//ひとつ前のボタンの状態
+int sts = -1;//ボタンを押したかどうかの状態を記憶
+//----------------------------------------------------------
+
 void setup() {
   // put your setup code here, to run once:
    Serial.begin(9600);
-   
+   pinMode(switchPin,INPUT);//ボタンは入力モード
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  val = digitalRead(switchPin);//ボタンが押されているかを確認する
+
 
   double Vin1 = voltage1 * Voffset1;
   double Vin2 = voltage2 * Voffset2;
-  
-  if (Vin1<=Vdue_th) {
-    analogWrite(outputPin1, val1);
-  } else {analogWrite(outputPin1, 0);}
-  
-  if (Vin2<=Vdue_th) {
-    analogWrite(outputPin2, val2);
-  } else {analogWrite(outputPin2, 0);}
 
+  if(val == HIGH && old_val == LOW){
+    sts = sts * (-1);//正数の場合にLEDを点灯、負数の場合は消灯
+    delay(50);//ハウシング対策
+  }
+
+  old_val = val;//ボタンの状態をひとつ前の状態として記憶とする
+
+
+  if(sts > 0){
+    if ((Vin1<=Vdue_th)&&(Vin2<=Vdue_th)){
+      analogWrite(outputPin1, val1);
+      analogWrite(outputPin2, val2);
+    } else {analogWrite(outputPin1, 0);
+            analogWrite(outputPin2, 0);
+      }
+  } else{analogWrite(outputPin1, 0);
+         analogWrite(outputPin2, 0);
+  }
   
   Serial.print("pin1 = ");
   Serial.print(outputPin1);
